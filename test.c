f5c7e6d8a65d78f6f9a912d9083cba761b72f036
kevin-harrison <kevin@harrisoncentral.com>
2019-12-06 13:49:26 +0100

    Added rotation and changed coords to floats

--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+const float PI = 3.1415926535;
+
 struct Point{
     int x;
     int y;
@@ -39,21 +41,61 @@ int random_int(int seed, int min, int max){
     return seed;
 }
 
+// cos estimation using taylor expansion
+float cos(float deg){
+    // convert to equivilant radian
+    if(deg<0) deg = -1*deg;
+    if(deg>360) deg -= ((int) (deg/360)) * 360; // mod 360
+    float rad = deg * PI / 180;
+
+    float res = 0;
+    float term = 1;
+    int k = 0;
+    while (res + term != res){
+        res += term;
+        k += 2;
+        term *= -1*rad*rad/k/(k-1);
+    }
+    return res;
+}
+
+// sin estimation using taylor expansion
+float sin(float deg){
+    // convert to equilivant radian
+    float sign = 1;
+    if(deg<0){
+        sign = -1;
+        deg = -1 * deg;
+    }
+    if(deg>360) deg -= ((int) deg/360) * 360; // mod 360
+    float rad = deg * PI / 180;
+
+    float res = 0;
+    float term = rad;
+    int k = 1;
+    while(res+term != res){
+        res += term;
+        k += 2;
+        term *= -1*rad*rad/k/(k-1);
+    }
+    return sign*res;
+}
+
 // draw pixels along line between 2 points
-void draw_line(int xstart, int ystart, int xend, int yend){
-    float slope = (float)(yend - ystart) / (float)(xend - xstart);
+void draw_line(float xstart, float ystart, float xend, float yend){
+    float slope = (yend - ystart) / (xend - xstart);
     int i;
     int yapprox, xapprox;
     float y; //holds current y value across iterations
     float x; //holds current x value across iterations
  
     // draw start and end pixels
-    scene[xstart][ystart] = 1;
-    scene[xend][yend] = 1;
+    scene[round(xstart)][round(ystart)] = 1;
+    scene[round(xend)][round(yend)] = 1;
 
     if(abs(xend-xstart) > abs(yend-ystart)){ // more lines between x
         if(xstart < xend){ // start at xstart and move forward
-            y = (float) ystart;
+            y = ystart;
             for(i = xstart+1; i < xend; i++){
                 y += slope;
                 yapprox = round(y);
@@ -61,7 +103,7 @@ void draw_line(int xstart, int ystart, int xend, int yend){
             }
         }
         else{ // start at xend and move backwards
-            y = (float) yend;
+            y = yend;
             for(i = xend+1; i < xstart; i++){
                 y += slope;
                 yapprox = round(y);
@@ -71,7 +113,7 @@ void draw_line(int xstart, int ystart, int xend, int yend){
     }
     else{ // more lines between y
         if(ystart < yend){ // start at ystart and move forwards
-            x = (float) xstart;
+            x = xstart;
             for(i = ystart+1; i < yend; i++){
                 x += 1 / slope;
                 xapprox = round(x);
@@ -79,7 +121,7 @@ void draw_line(int xstart, int ystart, int xend, int yend){
             }
         }
         else{ // start at yend and move backwards
-            x = (float) xend;
+            x = xend;
             for(i = yend+1; i < ystart; i++){
                 x += 1 / slope;
                 xapprox = round(x);
@@ -91,16 +133,42 @@ void draw_line(int xstart, int ystart, int xend, int yend){
 
 // Draws a shape defined as a list of points 
 // (draws a line from each point to the next in the array and finally from the last point to the first point)
-void draw_shape(int size, int pointarr[]){
+void draw_shape(int size, float pointarr[]){
     int i;
     for(i=0; i<(size*2)-2; i = i+2){ // draw a line from each point to the next
-        printf("(%d,%d) to (%d,%d)\n", i, i+1, i+2, i+3);
         draw_line(pointarr[i], pointarr[i+1], pointarr[i+2], pointarr[i+3]);
     }
     draw_line(pointarr[size*2-2], pointarr[size*2-1], pointarr[0], pointarr[1]); // draw line from last point to first
 }
 
 
+void rotate(float angle, float xcenter, float ycenter, int size, float* pointarr){
+    int i;
+    float xcentered;
+    float xnew;
+    float ynew;
+    float c = cos(angle);
+    float s = sin(angle);
+
+    for(i=0; i < size; i++){
+        xnew = *pointarr - xcenter;
+        xcentered = xnew;
+        pointarr += 1;
+        ynew = *pointarr - ycenter;
+        //pointarr -= 1;
+
+        xnew = (c*xnew) - (s*ynew);
+        ynew = (s*xcentered) + (c*ynew);
+
+        
+        printf("(%f, %f)\n", xnew + xcenter, ynew + ycenter);
+        *pointarr = ynew + ycenter;
+        pointarr -=1;
+        *pointarr = xnew + xcenter;
+
+        pointarr += 2; // point to next coord's x
+    }
+}
 
 
 int main(void){
@@ -114,14 +182,23 @@ int main(void){
     }
 
     // create and draw a square
-    int square[] = {0,0, 5,0, 5,5, 0,5};
+    float square[] = {0,0, 5,0, 5,5, 0,5};
     draw_shape(4, square);
     print_game(scene);
+    printf("\n\n");
 
-    int y;
-    for(y=1; y<10; y++){
-        printf("%d\n",random_int(i, 0, 2));
+
+
+    // erase screen
+    for(i=0; i<16; i++){
+        for(j=0; j<8; j++){
+            scene[i][j] = 0;
+        }
     }
+
+    rotate(45, 2.5, 2.5, 4, &square);
+    draw_shape(4, square);
+    print_game(scene);
 
     return 0;
 }
